2c6e58d24e6f66854ca1aa4ac3e6a52501755eed
my-choe <my_choe@naver.com>
2021-04-19 13:40:38 +0900

    191 확진자 위치에 마커 추가하기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*global kakao*/
+import React, { Component } from 'react';
+import jsonData from './map_info.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+
+  componentDidMount() {
+      var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+      mapOption = { 
+          center: new kakao.maps.LatLng(37.505496, 127.005116), // 지도의 중심좌표
+          level: 9 // 지도의 확대 레벨
+      };
+  
+    // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+    for(var i = 0; i < jsonData.positions.length ; i++){
+      var position = jsonData.positions[i];
+      var marker = new kakao.maps.Marker({
+        map: map,
+        position : new kakao.maps.LatLng(position.lat, position.lng),
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div id="map" style={{"width":"100%","height":"700px"}}></div>
+    )
+  }
 }
-
 export default App;
